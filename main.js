30ceb6f3fd30d01d7e0338e68f0db43e34ce66d2
Carlos Eduardo Da Cruz Costa <carloseduardodacruzcosta@gmail.com>
2022-01-15 14:30:57 -0300

    lista de tarefa

---
+++ main.js
@@ -0,0 +1,29 @@
+const tarefas = ['Estudar HTML', 'Estudar CSS', 'Estudar JS']
+
+
+function add() {
+    const ul = document.querySelector("ul")
+    const input = document.querySelector("input")
+    const tarefa = input.value
+
+    const li = document.createElement("li")
+    li.innerText = tarefa
+    ul.appendChild(li)
+    tarefa.push(tarefa)
+    input.value = ""
+    render()
+}
+
+function render() {
+    const ul = document.querySelector("ul")
+    ul.innerHTML = null
+    tarefas.forEach(function(tarefa) {
+        const li = document.createElement("li")
+        li.innerText = tarefa
+        ul.appendChild(li)
+    })
+
+}
+
+
+render()
